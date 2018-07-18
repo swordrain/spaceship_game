e9ada869b0b0ce3b2c40b1aa507e5152430c5a82
anli <anli.li1@ge.com>
2018-07-18 11:29:02 +0800

    spaceship move

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 class AlienGame {
     constructor(canvasDom) {
+        this.SPACESHIP_MOVESTEP = 8;
         this.canvas = canvasDom;
         this.context = this.canvas.getContext("2d");
-        this.spaceship = new Image();
-        this.alien = new Image();
+        this.spaceship = {
+            image: new Image()
+        };
+        this.alien = {
+            image: new Image()
+        };
         console.log(this.context);
     }
 
@@ -11,25 +16,53 @@ class AlienGame {
 
 
         Promise.all([new Promise((resolve, reject) => {
-            this.spaceship.src = "images/ship.bmp";
+            this.spaceship.image.src = "images/ship.bmp";
 
-            this.spaceship.onload = () => {
+            this.spaceship.image.onload = () => {
+
+                this.spaceship.location = {
+                    x: (this.canvas.width - this.spaceship.image.width) / 2,
+                    y: this.canvas.height - this.spaceship.image.height
+                }
                 resolve();
             };
 
         }), new Promise((resolve, reject) => {
-            this.alien.src = "images/alien.bmp";
+            this.alien.image.src = "images/alien.bmp";
 
-            this.alien.onload = () => {
+            this.alien.image.onload = () => {
                 resolve();
             };
         })]).then(() => {
+
+            window.addEventListener("keydown", e => {
+                switch (e.code) {
+                    case "ArrowRight":
+                        if (this.spaceship.location.x + this.spaceship.image.width + this.SPACESHIP_MOVESTEP >= this.canvas.width) {
+                            this.spaceship.location.x = this.canvas.width - this.spaceship.image.width;
+                        } else {
+                            this.spaceship.location.x += this.SPACESHIP_MOVESTEP;
+                        }
+
+                        break;
+                    case "ArrowLeft":
+                        if (this.spaceship.location.x - this.SPACESHIP_MOVESTEP <= 0) {
+                            this.spaceship.location.x = 0;
+                        } else {
+                            this.spaceship.location.x -= this.SPACESHIP_MOVESTEP;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            })
             this.draw();
         })
 
     }
 
     draw() {
+        console.log(this)
         let now = Date.now();
         requestAnimationFrame(() => {
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -51,7 +84,7 @@ class AlienGame {
 
     drawSpaceship() {
         this.context.save();
-        this.context.drawImage(this.spaceship, this.canvas.width / 2 - this.spaceship.width / 2, this.canvas.height - this.spaceship.height);
+        this.context.drawImage(this.spaceship.image, this.spaceship.location.x, this.spaceship.location.y);
         this.context.restore();
     }
 }
