bb30c0a88e45a9c0b6672245b948b6b79055eec1
anli <anli.li1@ge.com>
2018-07-18 10:53:08 +0800

    draw spaceship

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ class AlienGame {
     constructor(canvasDom) {
         this.canvas = canvasDom;
         this.context = this.canvas.getContext("2d");
+        this.spaceship = new Image();
+        this.alien = new Image();
         console.log(this.context);
     }
 
     init() {
-        this.draw();
+
+
+        Promise.all([new Promise((resolve, reject) => {
+            this.spaceship.src = "images/ship.bmp";
+
+            this.spaceship.onload = () => {
+                resolve();
+            };
+
+        }), new Promise((resolve, reject) => {
+            this.alien.src = "images/alien.bmp";
+
+            this.alien.onload = () => {
+                resolve();
+            };
+        })]).then(() => {
+            this.draw();
+        })
+
     }
 
     draw() {
@@ -14,6 +34,7 @@ class AlienGame {
         requestAnimationFrame(() => {
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
             this.drawBackground();
+            this.drawSpaceship();
 
             now = Date.now();
             this.draw();
@@ -23,8 +44,14 @@ class AlienGame {
 
     drawBackground() {
         this.context.save();
-        this.context.fillStyle = "black";
+        this.context.fillStyle = "rgb(230, 230, 230)";
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.restore();
+    }
+
+    drawSpaceship() {
+        this.context.save();
+        this.context.drawImage(this.spaceship, this.canvas.width / 2 - this.spaceship.width / 2, this.canvas.height - this.spaceship.height);
         this.context.restore();
     }
 }
